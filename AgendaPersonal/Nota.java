1c6a41a16369ac360966c9ea25b076e6848d6ea3
Maurcio Cauna <neymslionel.assi@gmail.com>
2022-10-11 11:20:15 -0400

    vercion 01 - de Agenda Personal

---
+++ AgendaPersonal/Nota.java
@@ -0,0 +1,16 @@
+
+public class Nota
+{
+    // instance variables - replace the example below with your own
+    private int id;
+    private String hora;
+    private String descrip;
+
+    public Nota()
+    {
+        // initialise instance variables
+        id = 0;
+    }
+
+    
+}
